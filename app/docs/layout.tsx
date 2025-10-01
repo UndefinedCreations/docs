1baa9b0b084af3d3af8e1a0b2bf1b7de57000135
StillLutto <still.lutto@gmail.com>
2025-10-01 08:19:10 +0200

    feat(v1.1.0): separate 1.0.5 and 1.1.0

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -2,7 +2,6 @@ import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
-import { GitBranch, GitBranchIcon, GitCommitIcon } from 'lucide-react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -11,7 +10,12 @@ export default function Layout({ children }: { children: ReactNode }) {
       sidebar={{
           tabs: [
             {
-              title: '1.0.5 (latest)',
+              title: '1.0.5',
+              // TODO add icon
+              url: '/docs/stellar/v1.0.5',
+            },
+            {
+              title: '1.1.0 (latest)',
               // TODO add icon
               url: '/docs/stellar/latest',
             },
