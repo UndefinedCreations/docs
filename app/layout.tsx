3fb5bfcd807ebf321359eead53f8b7719f4f9dda
StillLutto <still.lutto@gmail.com>
2025-12-12 16:57:11 +0100

    fix: fumadocs not updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
+import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
@@ -14,14 +14,14 @@ export default function Layout({ children }: { children: ReactNode }) {
           <title>Undefined Creations</title>
       </head>
       <body className="flex flex-col min-h-screen">
-      <RootProvider
-        theme={{
-          enabled: true,
-          forcedTheme: "dark"
-        }}
-      >
-        {children}
-      </RootProvider>
+        <RootProvider
+          theme={{
+            enabled: true,
+            forcedTheme: "dark"
+          }}
+        >
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
